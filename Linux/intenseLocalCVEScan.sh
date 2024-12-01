175eb81df1069de0157bf6ce69afb97e6b9d8171
HaloSam296 <halosam296@gmail.com>
2024-12-01 15:26:54 -0600

    quick bug fixes

--- Linux/intenseLocalCVEScan.sh
+++ Linux/intenseLocalCVEScan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -sV --script=vuln --script-args mincvss=8.0 --open p1-1024,300,3030,4443,6000,6060,8000,8080,9997 -T4 --max-retries 2 --max-rtt-timeout 300ms -T4 -n -oN intenseLocalScan.txt 127.0.0.1
+sudo nmap -sV --script=vuln --script-args mincvss=8.0 --open -p1-1024,300,3030,4443,6000,6060,8000,8080,9997 -T4 --max-retries 2 --max-rtt-timeout 300ms -T4 -n -oN scanResultsINTENSE.txt 127.0.0.1
