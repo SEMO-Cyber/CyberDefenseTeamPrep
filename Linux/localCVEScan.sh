e812a44d7d86c6c3b6ac3b690851f35f960723ee
HaloSam296 <halosam296@gmail.com>
2024-12-01 14:31:43 -0600

    edited cve scan script

--- Linux/localCVEScan.sh
+++ Linux/localCVEScan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo nmap -sV --script=vuln --script-args vulns.showall --open -p- -T4 -oN localhost_scan.txt 127.0.0.1
+sudo nmap -sV --script=vuln --script-args vulns.showall --open -p1-1024,3000-3100,6000-6100,6600-6700,8000-8100 -T4 -oN localhost_scan.txt 127.0.0.1
