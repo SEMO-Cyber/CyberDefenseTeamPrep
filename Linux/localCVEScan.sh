fcd6b515b85c0974a8975de28bba4fc0dc19feca
HaloSam296 <halosam296@gmail.com>
2024-12-01 15:24:24 -0600

    More nmap edits

--- Linux/localCVEScan.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo nmap -sV --script=vuln --script-args mincvss=8.0 --open -p1-1024,3000-3100,6000-6100,6600-6700,8000-8100 -T5 --max-retries 2 --max-rtt-timeout 300ms -T5 -n -oN quickLocalhostScan.txt 127.0.0.1
---
+++ Linux/quickLocalCVEScan.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo nmap -sV --script=vuln --script-args mincvss=8.0 --open p1-1024,300,3030,4443,6000,6060,8000,8080,9997 -T5 --max-retries 2 --max-rtt-timeout 300ms -T5 -n -oN quickLocalhostScan.txt 127.0.0.1
