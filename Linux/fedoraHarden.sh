e49ed5d107977cdc6853464ebd9c649ec096e799
HaloSam296 <halosam296@gmail.com>
2024-12-06 23:09:41 -0600

    added the dns rules i didnt forget about

--- Linux/fedoraHarden.sh
+++ Linux/fedoraHarden.sh
@@ -66,6 +66,14 @@ iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 # Allow Splunk forwarder traffic
 iptables -A OUTPUT -p tcp --dport 9997 -j ACCEPT
 
+# Allow outgoing DNS traffic
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+
+# Allow incoming DNS responses
+iptables -A INPUT -p udp --sport 53 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -m state --state ESTABLISHED -j ACCEPT
+
 # Log dropped packets
 iptables -A INPUT -j LOG --log-prefix "IPTABLES-DROP:" --log-level 4
 
