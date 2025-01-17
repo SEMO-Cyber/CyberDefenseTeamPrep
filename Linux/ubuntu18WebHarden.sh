ca4ae7b6aeef8ca01e0e5888d7997e15a218e236
IHotop7 <105679712+IHotop7@users.noreply.github.com>
2025-01-16 19:06:52 -0800

    Update ubuntu18WebHarden.sh

--- Linux/ubuntu18WebHarden.sh
+++ Linux/ubuntu18WebHarden.sh
@@ -43,11 +43,11 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 
 # Allow outgoing DNS traffic
-iptables -A OUTPUT -p udp --sport 53 -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 
 # Allow outgoing NTP traffic
-iptables -A OUTPUT -p udp --sport 123 -j ACCEPT
+iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
 
 # Allow Splunk forwarder traffic
 iptables -A OUTPUT -p tcp --sport 9997 -j ACCEPT
