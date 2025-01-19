a9cca1015bdc77d86e5afe836766ee436966e3b1
callmesas <32786439+shaswatn05@users.noreply.github.com>
2025-01-19 14:57:10 -0600

    Update ubuntu18WebHarden.sh
    Allowing WEB traffic

--- Linux/ubuntu18WebHarden.sh
+++ Linux/ubuntu18WebHarden.sh
@@ -39,10 +39,15 @@ iptables -A OUTPUT -o lo -j ACCEPT
 
 # Allow incoming HTTP traffic
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+# iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
 # Allow outgoing DNS traffic
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+
+# Allow outgoing WEB traffic
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 
 # Allow outgoing NTP traffic
 iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
