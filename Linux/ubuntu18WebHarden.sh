c045d37d669276dac10fac0f2a881b1087106609
IHotop7 <105679712+IHotop7@users.noreply.github.com>
2025-01-26 14:22:07 -0600

    Update ubuntu18WebHarden.sh

--- Linux/ubuntu18WebHarden.sh
+++ Linux/ubuntu18WebHarden.sh
@@ -29,6 +29,16 @@ echo "Configuring firewall rules..."
 iptables -F
 iptables -X
 
+# Allow limited incomming ICMP traffic and log packets that dont fit the rules
+sudo iptables -A INPUT -p icmp --icmp-type echo-request -m length --length 0:192 -m limit --limit 1/s --limit-burst 5 -j ACCEPT
+sudo iptables -A INPUT -p icmp --icmp-type echo-request -m length --length 0:192 -j LOG --log-prefix "Rate-limit exceeded: " --log-level 4
+sudo iptables -A INPUT -p icmp --icmp-type echo-request -m length ! --length 0:192 -j LOG --log-prefix "Invalid size: " --log-level 4
+sudo iptables -A INPUT -p icmp --icmp-type echo-reply -m limit --limit 1/s --limit-burst 5 -j ACCEPT
+sudo iptables -A INPUT -p icmp -j DROP
+
+# Allow outgoing ICMP traffic
+sudo iptables -A OUTPUT -p icmp -j ACCEPT
+
 # Allow traffic from existing/established connections
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
