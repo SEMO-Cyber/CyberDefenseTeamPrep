b073af0641e089161a07fb9638c907a120defafc
HaloSam296 <halosam296@gmail.com>
2025-01-26 21:01:52 -0600

    Update DoNotRun__splunkInstall.sh

--- Splunk/DoNotRun__splunkInstall.sh
+++ Splunk/DoNotRun__splunkInstall.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-#super quick automatic script to install Splunk. Made with a heavy sprinkle of AI
+#super quick automatic script to install Splunk 9.3.2
+# Mostly original with a little sprinkle of AI
+# Samuel Brucker 2024-2025
 
 # Define variables
 SPLUNK_URL="https://download.splunk.com/products/splunk/releases/9.3.2/linux/splunk-9.3.2-d8bb32809498.x86_64.rpm"
