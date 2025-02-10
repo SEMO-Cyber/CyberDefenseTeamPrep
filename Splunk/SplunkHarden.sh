51770ad18eb0cb054806ca6822555252d9f37696
SEMO-Cyber <99694954+SEMO-Cyber@users.noreply.github.com>
2025-02-09 20:58:53 -0600

    Update SplunkHarden.sh

--- Splunk/SplunkHarden.sh
+++ Splunk/SplunkHarden.sh
@@ -231,7 +231,7 @@ echo "Hardening the Splunk configuration..."
 cat > "$SPLUNK_HOME/etc/system/local/global-banner.conf" << EOF
 [BANNER_MESSAGE_SINGLETON]
 global_banner.visible = true
-global_banner.message = "WARNING: NO UNAUTHORIZED ACCESS. This is property of Team 10 LLC. Unauthorized users will be prosecuted and tried to the furthest extent of the law. By accessing this system, you consent to constant monitoring and logging of your activities. DO NOT PROCEED FURTHER UNLESS AUTHORIZED BY A TEAM10 LLC ADMIN!"
+global_banner.message = WARNING: NO UNAUTHORIZED ACCESS. This is property of Team 10 LLC. Unauthorized users will be prosecuted and tried to the furthest extent of the law!
 global_banner.background_color = red
 EOF
 
