f57d2e61c0a528cb720c951e58ee510c07bb1604
SEMO-Cyber <99694954+SEMO-Cyber@users.noreply.github.com>
2025-02-11 15:35:55 -0600

    Update fedoraHarden.sh

--- Linux/fedoraHarden.sh
+++ Linux/fedoraHarden.sh
@@ -9,6 +9,12 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+echo "Setting device banner"
+cat > /etc/issue << EOF
+LEGAL DISCLAIMER: This computer system is the property of Team 10 LLC. By using this system, all users acknowledge notice of, and agree to comply with, the Acceptable User of Information Technology Resources Polity (AUP). 
+By using this system, you consent to these terms and conditions. Use is also consent to monitoring, logging, and use of logging to prosecute abuse. 
+If you do NOT wish to comply with these terms and conditions, you must LOG OFF IMMEDIATELY.
+EOF
 
 # Determine package manager
 if command -v dnf &> /dev/null; then
