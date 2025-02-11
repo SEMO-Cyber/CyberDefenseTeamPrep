6982dfc9acde4a80c5814d9ed0e3f6ad82f4a551
SEMO-Cyber <99694954+SEMO-Cyber@users.noreply.github.com>
2025-02-11 15:34:54 -0600

    Update SplunkHarden.sh

--- Splunk/SplunkHarden.sh
+++ Splunk/SplunkHarden.sh
@@ -30,7 +30,7 @@ echo "Starting the basic hardening."
 
 echo "Setting device banner"
 cat > /etc/issue << EOF
-LEGAL DISCLAIMER: This computer system is the property of Team [team number] LLC. By using this system, all users acknowledge notice of, and agree to comply with, the Acceptable User of Information Technology Resources Polity (AUP). 
+LEGAL DISCLAIMER: This computer system is the property of Team 10 LLC. By using this system, all users acknowledge notice of, and agree to comply with, the Acceptable User of Information Technology Resources Polity (AUP). 
 By using this system, you consent to these terms and conditions. Use is also consent to monitoring, logging, and use of logging to prosecute abuse. 
 If you do NOT wish to comply with these terms and conditions, you must LOG OFF IMMEDIATELY.
 EOF
