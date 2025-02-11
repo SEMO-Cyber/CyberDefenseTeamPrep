28ea4bb24868ac887686099c7a51e2c7c7f374d1
SEMO-Cyber <99694954+SEMO-Cyber@users.noreply.github.com>
2025-02-11 10:36:05 -0600

    Update CentOS-Upgrade.sh

--- Linux/CentOS-Upgrade.sh
+++ Linux/CentOS-Upgrade.sh
@@ -4,12 +4,13 @@
 #
 #  Samuel Brucker 2024 - 2025
 
-sudo dnf update
+sudo yum update
 sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-sudo dnf install centos-release-stream
-sudo dnf swap centos-{linux,stream}-repos
+sudo yum install centos-release-stream
+sudo yum swap centos-{linux,stream}-repos
 
-sudo dnf distro-sync
+sudo yum distro-sync
 
+echo "Migration complete. RESTART THE SYSTEM!!!"
