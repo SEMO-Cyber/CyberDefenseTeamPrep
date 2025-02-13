33ae661bc9194b3b8e8b43f52fcba74620a23cf9
SEMO-Cyber <99694954+SEMO-Cyber@users.noreply.github.com>
2025-02-13 17:27:22 -0600

    Delete Linux/intenseLocalCVEScan.sh

--- Linux/intenseLocalCVEScan.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo nmap --script=vuln --script-args mincvss=7.0 --open -p1-1000,3000,3030,6000,6060,8000,8080,9997 -T4 --max-retries 2 --max-rtt-timeout 300ms -T4 -n -oN scanResultsINTENSE.txt 127.0.0.1
