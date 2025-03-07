16ecd4ff3dde1c003dd46ce3479f4d8863428f9c
rtsPraveenNaidu <koppulapraveenkumar920@gmail.com>
2025-03-06 21:45:48 -0600

    Create icheckthread.sh

---
+++ Linux/icheckthread.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+LOG_FILE="/var/log/integrity_monitor.log"
+
+log_event() {
+    local message="$1"
+    local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$timestamp] [ALERT] $message" >> "$LOG_FILE"
+}
+
+monitor_immutable() {
+    while true; do
+        for dir in $(awk '{print $3}' configuration.txt); do
+            if [[ $(lsattr -d "$dir" | awk '{print $1}') != "----i--------" ]]; then
+                log_event "Immutable flag removed from $dir. Restoring..."
+                chattr +i "$dir"
+                log_event "Restored immutable flag for $dir"
+            fi
+        done
+        sleep 5
+    done
+}
+
+monitor_immutable
