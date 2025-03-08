55c563c1275a6efbf38daaa4e77393851b27044a
rtsPraveenNaidu <koppulapraveenkumar920@gmail.com>
2025-03-07 19:26:28 -0600

    Update CreateConfigAndLogs.sh

--- Linux/Cm/CreateConfigAndLogs.sh
+++ Linux/Cm/CreateConfigAndLogs.sh
@@ -15,6 +15,16 @@ LOG_FILES=(
     "/var/log/malicious_keys.log"
 )
 
+# Define the sub-scripts that need to be made executable
+SCRIPTS=(
+    "./IntegrityCheck.sh"
+    "./ServiceManager.sh"
+    "./KeySearch.sh"
+    "./BackupHandler.sh"
+    "./ConfigManager.sh"
+    "./CustomManager.sh"
+)
+
 # Function to create an empty file if it doesn't exist
 create_file() {
     FILE=$1
@@ -23,6 +33,17 @@ create_file() {
         echo "$FILE has been created."
     else
         echo "$FILE already exists."
+    fi
+}
+
+# Function to set executable permissions for scripts
+set_executable_permissions() {
+    SCRIPT=$1
+    if [ -f "$SCRIPT" ]; then
+        chmod +x "$SCRIPT"
+        echo "Executable permissions set for $SCRIPT."
+    else
+        echo "$SCRIPT does not exist."
     fi
 }
 
@@ -38,4 +59,10 @@ for LOG_FILE in "${LOG_FILES[@]}"; do
     create_file "$LOG_FILE"
 done
 
-echo "All necessary .config and .log files have been created."
+# Set executable permissions for the sub-scripts
+echo "Setting executable permissions for sub-scripts..."
+for SCRIPT in "${SCRIPTS[@]}"; do
+    set_executable_permissions "$SCRIPT"
+done
+
+echo "All necessary .config, .log files have been created, and executable permissions have been set for sub-scripts."
